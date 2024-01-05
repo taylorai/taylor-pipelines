738b4064e881f49e0fcc759d11a2d2f69c80b329
Benjamin Anderson <benjamin@Benjamins-MacBook-Pro.local>
2024-01-05 13:27:09 -0800

    better metrics, val split

--- src/taylor_pipelines/classification.py
+++ src/taylor_pipelines/classification.py
@@ -7,6 +7,7 @@ from functools import partial
 from sklearn.linear_model import PassiveAggressiveClassifier, SGDClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 from collections import Counter
 from .embeddings import ONNXEmbeddingModel
 import joblib
@@ -28,6 +29,7 @@ class TrainClassifier(Map):
         balanced: bool = False,
         epochs: int = 1,
         model: Literal["passive_aggressive", "logistic_regression", "mlp"] = "passive_aggressive",
+        validation_split: float = 0.1,
         output_directory: Optional[str] = None, # will use name if not provided
         optional: bool = False,
     ):
@@ -50,9 +52,14 @@ class TrainClassifier(Map):
             kwargs["class_weight"] = "balanced"
         self.model = NAME_TO_MODEL[model](**kwargs)
         self.iters = 0
-        self.metrics = {"accuracy": [], "per_class": []}
+        self.metrics = {
+            "accuracy": [], 
+            "per_class": [],
+            "val_accuracy": [],
+            "val_per_class": [],
+        }
         self.epochs = epochs
-        self.data = {"X": [], "y": []}
+        self.data = {"X_train": [], "y_train": [], "X_val": [], "y_val": []}
         self.batch_size = None
 
     def compile(self, **kwargs):
@@ -60,83 +67,36 @@ class TrainClassifier(Map):
 
     def print_metrics(self):
         print(f"=== Classification Metrics for {self.name} ===")
-        print(f"Accuracy on Last (Full) Batch: {self.metrics['accuracy'][-2]:.3f}")
-        for cls in self.label2idx:
-            print(f"[Metrics for {cls}]")
-            print(f"  ↳ Precision: {self.metrics['per_class'][-2][cls]['precision']:.3f}")
-            print(f"  ↳ Recall: {self.metrics['per_class'][-2][cls]['recall']:.3f}")
-            print(f"  ↳ F1: {self.metrics['per_class'][-2][cls]['f1-score']:.3f}")
 
-    async def map(self, batch: list[dict], executor: concurrent.futures.Executor):
-        if self.batch_size is None:
-            self.batch_size = len(batch)
-        X = [entry[self.input_field] for entry in batch]
-        y = [self.label2idx[entry[self.label_field]] for entry in batch]
-        try:
-            y_pred = self.model.predict(X)
-            accuracy = sum(y_pred == y) / len(y)
-            report = classification_report(y, y_pred, output_dict=True, zero_division=np.nan)
-            self.metrics["per_class"].append({
-                label: report[str(self.label2idx[label])] for label in self.label2idx
-            })
-            self.metrics["accuracy"].append(accuracy)
-        except:
-            pass
-        self.model.partial_fit(X, y, classes=range(len(self.label2idx)))
-        self.iters += 1
-
-        # save data if doing >1 epoch
+        # if multi-epoch, report validation metrics
         if self.epochs > 1:
-            self.data["X"].extend(X)
-            self.data["y"].extend(y)
-        
-        output_file = f"{self.output_directory}/{self.name}_model.joblib"
-        # check if output director exists (it's NFS so if we don't check, os.makedirs will fail)
-        if not os.path.exists(self.output_directory):
-            os.makedirs(self.output_directory)
-        # each iteration will overwrite the previous model
-        joblib.dump({
-            "model": self.model, 
-            "idx2label": self.idx2label,
-        }, f"{output_file}")
+            print("Validation accuracy for each epoch:")
+            print([round(acc, 2) for acc in self.val_accuracy])
+            print(f"Final validation accuracy: {self.val_accuracy[-1]:.3f}")
+            
+            for cls in self.label2idx:
+                print(f"[Final Val Metrics for {cls}]")
+                print(f"  ↳ Precision: {self.val_per_class[-1][cls]['precision']:.3f}")
+                print(f"  ↳ Recall: {self.val_per_class[-1][cls]['recall']:.3f}")
+                print(f"  ↳ F1: {self.val_per_class[-1][cls]['f1-score']:.3f}")
 
-        return batch
+        # otherwise, report metrics on last batch
+        else:
+            print(f"Accuracy on Last Batch: {self.metrics['accuracy'][-1]:.3f}")
+            for cls in self.label2idx:
+                print(f"[Metrics for {cls}]")
+                print(f"  ↳ Precision: {self.metrics['per_class'][-1][cls]['precision']:.3f}")
+                print(f"  ↳ Recall: {self.metrics['per_class'][-1][cls]['recall']:.3f}")
+                print(f"  ↳ F1: {self.metrics['per_class'][-1][cls]['f1-score']:.3f}")
+
+    def evaluate(self, X, y):
+        y_pred = self.model.predict(X)
+        accuracy = sum(y_pred == y) / len(y)
+        report = classification_report(y, y_pred, output_dict=True, zero_division=np.nan)
+        per_class = {label: report[str(self.label2idx[label])] for label in self.label2idx}
+        return accuracy, per_class
     
-    def complete_remaining_epochs(self):
-        print(f"=== Training {self.name} for {self.epochs - 1} more epochs ===")
-        # shape check
-        print("X shape: ", np.array(self.data["X"]).shape)
-        print("y shape: ", np.array(self.data["y"]).shape)
-        if self.epochs == 1:
-            return
-        for ep in range(self.epochs - 1):
-            # shuffle the data
-            idxs = np.arange(len(self.data["X"]))
-            np.random.shuffle(idxs)
-            X = [self.data["X"][i] for i in idxs]
-            y = [self.data["y"][i] for i in idxs]
-            # another shape check
-            print("X shape: ", np.array(X).shape)
-            print("y shape: ", np.array(y).shape)
-            # fit the model
-            for i in range(0, len(X), self.batch_size):
-                X_batch, y_batch = X[i:i+self.batch_size], y[i:i+self.batch_size]
-                # yet another shape check
-                print("X_batch shape: ", np.array(X_batch).shape)
-                print("y_batch shape: ", np.array(y_batch).shape)
-                try:
-                    y_pred = self.model.predict(X_batch)
-                    accuracy = sum(y_pred == y_batch) / len(y_batch)
-                    report = classification_report(y_batch, y_pred, output_dict=True, zero_division=np.nan)
-                    self.metrics["per_class"].append({
-                        label: report[str(self.label2idx[label])] for label in self.label2idx
-                    })
-                    self.metrics["accuracy"].append(accuracy)
-                except Exception as e:
-                    print("Error:", e)
-                    pass
-                self.model.partial_fit(X_batch, y_batch, classes=range(len(self.label2idx)))
-                self.iters += 1
+    def save_model(self):
         # save the model
         output_file = f"{self.output_directory}/{self.name}_model.joblib"
         # check if output director exists (it's NFS so if we don't check, os.makedirs will fail)
@@ -148,6 +108,68 @@ class TrainClassifier(Map):
             "idx2label": self.idx2label,
         }, f"{output_file}")
 
+    async def map(self, batch: list[dict], executor: concurrent.futures.Executor):
+        if self.batch_size is None:
+            self.batch_size = len(batch)
+        X = [entry[self.input_field] for entry in batch]
+        y = [self.label2idx[entry[self.label_field]] for entry in batch]
+        
+        # train test split for the batch (only if >1 epoch)
+        if self.epochs > 1:
+            X, X_val, y, y_val = train_test_split(X, y, test_size=self.validation_split)
+            # save validation data for later
+            self.data["X_val"].extend(X_val) 
+            self.data["y_val"].extend(y_val)
+
+            # add training data to the data dict
+            self.data["X_train"].extend(X)
+            self.data["y_train"].extend(y)
+
+        # metrics on train data
+        accuracy, per_class = self.evaluate(X, y)
+        self.metrics["per_class"].append(per_class)
+        self.metrics["accuracy"].append(accuracy)
+
+        # fit the model
+        self.model.partial_fit(X, y, classes=range(len(self.label2idx)))
+        self.iters += 1
+        
+        # save the model
+        self.save_model()
+        return batch
+    
+    def complete_remaining_epochs(self):
+        if self.epochs == 1:
+            return
+        
+        # calculate initial validation metrics
+        X_val = self.data["X_val"]
+        y_val = self.data["y_val"]
+        accuracy, per_class = self.evaluate(X_val, y_val)
+        self.metrics["val_per_class"].append(per_class)
+        self.metrics["val_accuracy"].append(accuracy)
+
+        print(f"=== Training {self.name} for {self.epochs - 1} more epochs ===")
+        for _ in range(self.epochs - 1):
+            # shuffle the data
+            idxs = np.arange(len(self.data["X_train"]))
+            np.random.shuffle(idxs)
+            X = [self.data["X_train"][i] for i in idxs]
+            y = [self.data["y_train"][i] for i in idxs]
+            
+            # fit the model
+            for i in range(0, len(X), self.batch_size):
+                X_batch, y_batch = X[i:i+self.batch_size], y[i:i+self.batch_size]
+                self.model.partial_fit(X_batch, y_batch, classes=range(len(self.label2idx)))
+                self.iters += 1
+
+            # calculate validation metrics
+            accuracy, per_class = self.evaluate(X_val, y_val)
+            self.metrics["val_per_class"].append(per_class)
+            self.metrics["val_accuracy"].append(accuracy)
+
+            # save the model
+            self.save_model()
 
 class Classifier(Map):
     def __init__(
